a112a12191c13fd0c51b5533662ffc5311525b32
noumecha <noumechaivan@gmail.com>
2022-06-03 17:07:52 +0100

    maj : some maj -tmc

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React , {useState} from 'react'
+import { render } from 'react-dom'
+
+function Compteur () 
+{
+    const state = useState(0)
+    console.log(state)
+    return <button></button>
+}
+
+render (
+    <div>
+        <Compteur />
+    </div>,
+    document.getElementById('app')
+)
